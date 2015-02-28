9c8a10ceb4909fa3328b4a3b047f0cdd3d6c9632
Matthew <Matthew@Matthew-Curtners-iMac.local>
2015-02-28 20:00:11 +0100

    Changes to ViewController

--- toDegrees/ViewController.swift
+++ toDegrees/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var celsiusImageView = UIImageView()
+    var fahrenheitImageView = UIImageView()
+    
     var celsiusLabel: UILabel! = UILabel()
     var fahrenheitLabel: UILabel! = UILabel()
-    var swipeToClearLabel: UILabel! = UILabel()
-    
+
     var useFont = UIFont(name: "HelveticaNeue-Thin", size: 50)
     var textColor = UIColor.whiteColor()
-
     
-
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
@@ -26,25 +27,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //Create the UIImageViews
-        let celsiusImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width/2, height: self.view.frame.size.height))
-        let fahrenheitImageView = UIImageView(frame: CGRect(x: self.view.frame.size.width/2, y: 0.0, width: self.view.frame.size.width/2, height: self.view.frame.size.height))
-        let swipeImageView = UIImageView(frame: CGRect(x: 0.0, y: self.view.frame.height / 1.1 , width: self.view.frame.size.width, height: 50.0))
-
-        
+        celsiusImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width/2, height: self.view.frame.size.height))
+        fahrenheitImageView = UIImageView(frame: CGRect(x: self.view.frame.size.width/2, y: 0.0, width: self.view.frame.size.width/2, height: self.view.frame.size.height))
+    
         //Enable user interaction for Image Views
         fahrenheitImageView.userInteractionEnabled = true
         celsiusImageView.userInteractionEnabled = true
-        swipeImageView.userInteractionEnabled = true
         
         //Create the labels
         celsiusLabel =  UILabel(frame: CGRectMake(5.0, celsiusImageView.frame.height/2, celsiusImageView.frame.width - 10, 50))
         fahrenheitLabel = UILabel(frame: CGRectMake(fahrenheitImageView.frame.size.width + 10, fahrenheitImageView.frame.height/2, fahrenheitImageView.frame.size.width - 10, 50))
-        swipeToClearLabel = UILabel(frame: CGRectMake(5.0, swipeImageView.frame.size.height/2, swipeImageView.frame.size.width/2, swipeImageView.frame.size.height/2))
+        
+        //Display label and offsets
+        fahrenheitLabel.shadowColor = UIColor.blackColor()
+        fahrenheitLabel.shadowOffset = CGSizeMake(1.0, -1.0)
+        celsiusLabel.shadowColor = UIColor.blackColor()
+        celsiusLabel.shadowOffset = CGSizeMake(1.0, -1.0)
+        
         
         //Set the background colors
-        celsiusImageView.backgroundColor = UIColor.blueColor()
-        fahrenheitImageView.backgroundColor = UIColor.redColor()
-        swipeImageView.backgroundColor = UIColor.blackColor()
+        celsiusImageView.backgroundColor = UIColor(hue: 0.610, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        fahrenheitImageView.backgroundColor = UIColor(hue: (64 + 112) / 255.0, saturation: 1.0, brightness: 1.0, alpha: 1.0)
         
         celsiusLabel.text = "0"
         celsiusLabel.textAlignment = NSTextAlignment.Center
@@ -56,24 +59,10 @@ class ViewController: UIViewController {
         fahrenheitLabel.font = useFont
         fahrenheitLabel.textColor = textColor
         
-        swipeToClearLabel.text = "Swipe to Clear"
-        swipeToClearLabel.textAlignment = NSTextAlignment.Center
-        swipeToClearLabel.font = useFont
-        swipeToClearLabel.textColor = textColor
-        
-        
         view.addSubview(celsiusImageView)
         view.addSubview(fahrenheitImageView)
-        view.addSubview(swipeImageView)
         view.addSubview(celsiusLabel)
         view.addSubview(fahrenheitLabel)
-        swipeImageView.addSubview(swipeToClearLabel)
-        
-        //Reset gesture recognizer
-        let swipeSelector: Selector = "swipeToReset:"
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: swipeSelector)
-        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
-        swipeImageView.addGestureRecognizer(swipeRight)
         
         //Pan Gesture
         let panFahrenGesture:Selector = "setFahrenheitTemp:"
@@ -123,7 +112,6 @@ class ViewController: UIViewController {
     
     
     func setFahrenheitTemp(recognizer:UIPanGestureRecognizer) {
-
         var translation: CGPoint = recognizer.translationInView(recognizer.view!)
         
         if (translation.y < -20 || translation.y > 20) {
@@ -140,6 +128,11 @@ class ViewController: UIViewController {
             //Add one to value and
             var increasedValue = (currentFahrValue + Float(sign * 1))
             
+            //Update UIImageView background color
+            fahrenheitImageView.backgroundColor = setFahrenTempBackgroundColor(temperature: increasedValue.f)
+            celsiusImageView.backgroundColor = setCelsiusTempBackgroundColor(temperature: increasedValue.f)
+
+            
             //Display Fahrenheit value
             fahrenheitLabel.text = convertToStringValue(increasedValue)
             
@@ -148,12 +141,13 @@ class ViewController: UIViewController {
             
             //Display celsius value
             celsiusLabel.text = convertToStringValue(celsiusValue)
+            celsiusLabel.shadowColor = UIColor.blackColor()
+            celsiusLabel.shadowOffset = CGSizeMake(1.0, -1.0)
         }
     }
     
     
     func setCelsiusTemp(recognizer:UIPanGestureRecognizer) {
-        
         var translation: CGPoint = recognizer.translationInView(recognizer.view!)
         
         if (translation.y < -20 || translation.y > 20) {
@@ -170,6 +164,11 @@ class ViewController: UIViewController {
             //Add one to value and
             var increasedValue = (currentCelsiusValue + Float(sign * 1))
             
+            //Update UIImageView background color
+            fahrenheitImageView.backgroundColor = setFahrenTempBackgroundColor(temperature: 32.0 + (increasedValue.f * 1.800))
+            celsiusImageView.backgroundColor = setCelsiusTempBackgroundColor(temperature: 32.0 + (increasedValue.f * 1.800))
+            
+            
             //Display Fahrenheit value
             celsiusLabel.text = convertToStringValue(increasedValue)
             
@@ -178,15 +177,27 @@ class ViewController: UIViewController {
             
             //Display celsius value
             fahrenheitLabel.text = convertToStringValue(fahrenheitValue)
+            fahrenheitLabel.shadowColor = UIColor.blackColor()
+            fahrenheitLabel.shadowOffset = CGSizeMake(1.0, -1.0)
         }
     }
     
     
     //MARK: Change UIImageView Background Color
-    //TODO
+    func setFahrenTempBackgroundColor(#temperature: CGFloat) -> UIColor {
+        var num = (-temperature + 112) / 255.0
+        var backgroundColor: UIColor = UIColor(hue: num + num, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        
+        return backgroundColor
+    }
     
-    
-    
+    func setCelsiusTempBackgroundColor(#temperature: CGFloat) -> UIColor {
+        var num = (-temperature + 110) / 255.0
+        println(num)
+        var backgroundColor: UIColor = UIColor(hue: num + num, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+
+        return backgroundColor
+    }
     
 }
 
@@ -196,3 +207,6 @@ extension String {
     }
 }
 
+extension Float {
+    var f: CGFloat {return CGFloat(self)}
+}
